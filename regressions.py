643178ed458aa74d938728aa6304db047803cced
Areasus <cantosh.s@gmail.com>
2024-08-22 13:53:16 +0545

    added regressions

---
+++ regressions.py
@@ -0,0 +1,3 @@
+# Linear regression uses the relationship between the data-points to draw a straight line through all them.
+# Polynomial regression uses the relationship between the variables x and y to find the best way to draw a line through the  points.
+# Multiple regression is like linear regression, but with more than one independent value, meaning predict a value based on two or more variables.
